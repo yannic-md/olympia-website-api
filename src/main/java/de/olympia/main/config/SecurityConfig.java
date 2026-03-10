2bf64a77d782356c970fd8ad28c6529f7fd51730
Yannic <r4zzeryt@gmail.com>
2026-03-10 15:45:13 +0100

    fix: allow builded & dev versions of angular to API

--- src/main/java/de/olympia/main/config/SecurityConfig.java
+++ src/main/java/de/olympia/main/config/SecurityConfig.java
@@ -69,7 +69,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:4200"));
+        configuration.setAllowedOrigins(List.of("http://localhost:4200", "http://localhost:4000"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
